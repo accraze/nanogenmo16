ffd2f050f50cf4a0fe6e97b653a5a0508c98d568
accraze <accraze@gmail.com>
2016-11-20 17:07:07 -0800

    refactor(markov): pass archive name to train method

--- markov.py
+++ markov.py
@@ -3,46 +3,44 @@ import markovify
 import glob
 import os
 
-ia_corpus_names = [
-    'flatlandromanceo00abbouoft',
-    'YouDontNeedAWeathermanToKnowWhichWayTheWindBlows_925',
-    'FuturistManifesto',
-    'TheSilverPanthersOfLunaAVisualCompanion',
-    'btthg31.pdf',
-    'al_FC_Industrial_Society_and_Its_Future_a4'
-]
-
-
-
 
 class MarkovModel(object):
-  
-  def __init__(self, archive_name):
-      self.archive_name = archive_name
-      self.model = None
 
-  def train_model(self):
-    self._download_corpus()
-    self._unpack_corpus()
+    """
+    A Markov Model trained on
+    on an Internet Archive text file.
+    """
 
-  def _download_corpus(self):
-    """
-    Downloads a corpus of text
-    from internet archive to 
-    current working directory
-    """
-    download(self.archive_name, verbose=True, glob_pattern="*.txt")
+    def __init__(self):
+        self.archive_name = None
+        self.model = None
 
+    def train_model(self, archive_name):
+        """
+        Trains the model
+        on a given archive_name
+        """
+        self.archive_name = archive_name
+        self._download_corpus()
+        self._unpack_corpus()
 
-  def _unpack_corpus(self):
-    """
-    unpacks a text file
-    downloaded from internet archive
-    """
-    filenames = glob.glob(os.path.join(self.archive_name, '*.txt'))
-    text = None
-    for filename in filenames:
-      with open(filename) as f:
-        text = f.read()
-      # build the model
-      self.model = markovify.Text(text)
+    def _download_corpus(self):
+        """
+        Downloads a corpus of text
+        from internet archive to 
+        current working directory
+        """
+        download(self.archive_name, verbose=True, glob_pattern="*.txt")
+
+    def _unpack_corpus(self):
+        """
+        unpacks a text file
+        downloaded from internet archive
+        """
+        filenames = glob.glob(os.path.join(self.archive_name, '*.txt'))
+        text = None
+        for filename in filenames:
+            with open(filename) as f:
+                text = f.read()
+            # build the model
+            self.model = markovify.Text(text)
