d12485777bdce5f35bc2b65f9d2e4e554a24b1f0
accraze <accraze@gmail.com>
2016-11-24 11:19:28 -0800

    chore(novel): rough draft generated

--- generate_novel.py
+++ generate_novel.py
@@ -1,9 +1,42 @@
-from markov import MarkovModel
+from markov import MarkovModel, POSMarkov
+import markovify
 from novel import Novel
+import random
 
-m = MarkovModel()
-m.train_model('FuturistManifesto')
+ia_archives = [
+  'FuturistManifesto',
+  'flatlandromanceo00abbouoft',
+  'YouDontNeedAWeathermanToKnowWhichWayTheWindBlows_925',
+  'btthg31.pdf',
+  'al_FC_Industrial_Society_and_Its_Future_a4',
+  'VaneigemTheRevolutionOfEverydayLife',
+  'DebordSocietyOfTheSpectacleDonaldNicholsonSmithTranslation',
+  'TheGeometry'
+]
 
-novel = Novel(m.model, chapter_count=3)
-novel.write(novel_title='mynovel')
+markov_models = []
+
+for archive_name in ia_archives:
+  m = POSMarkov()
+  m.train_model(archive_name)
+  markov_models.append(m.model)
+
+# pick random weights for models
+weights = []
+for m in markov_models:
+ weights.append(random.uniform(0.5, 2.0)) 
+
+markov_combo = markovify.combine(markov_models, weights)
+
+novel = Novel(markov_combo, chapter_count=10)
+novel.write(novel_title='mynovel', filetype='md')
+
+file = open('mynovel.md', 'r+')
+word_count = 0
+for word in file.read().split():
+  word_count = word_count + 1
+file.close()
+
+print ('Total word count: %d' % word_count)
+
 
