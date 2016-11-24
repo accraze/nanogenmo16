cb07658c4ab776e0ac26bef889f7be858931c9e8
accraze <accraze@gmail.com>
2016-11-24 10:29:15 -0800

    style(markov): pep8 cleanup

--- markov.py
+++ markov.py
@@ -48,43 +48,39 @@ class MarkovModel(object):
             self._create_markov(text)
 
     def _create_markov(self, text):
-      """
-      Assign markovify.Text 
-      as model
-      """
-      self.model = markovify.Text(text)
-
+        """
+        Assign markovify.Text 
+        as model
+        """
+        self.model = markovify.Text(text)
 
 
 class POSMarkov(MarkovModel):
 
     def _create_markov(self, text):
-      """
-      Assign part of speech tagged markov
-      to model.
-      """
-      self.model = POSifiedText(text)
-
+        """
+        Assign part of speech tagged markov
+        to model.
+        """
+        self.model = POSifiedText(text)
 
 
 class POSifiedText(markovify.Text):
-  """
-  Override Markovify Text
-  to use part-of-speech tagging
-  on training text.
-  """
-  def word_split(self, sentence):
-    words = re.split(self.word_split_pattern, sentence)
-    print words
-    try:
-      words = [ "::".join(tag) for tag in nltk.pos_tag(words)]
-    except:
-      pass
-    return words
+    """
+    Override Markovify Text
+    to use part-of-speech tagging
+    on training text.
+    """
 
-  def word_join(self, words):
-    sentence = " ".join(word.split("::")[0] for word in words)
-    return sentence
+    def word_split(self, sentence):
+        words = re.split(self.word_split_pattern, sentence)
+        print words
+        try:
+            words = ["::".join(tag) for tag in nltk.pos_tag(words)]
+        except:
+            pass
+        return words
 
-
-
+    def word_join(self, words):
+        sentence = " ".join(word.split("::")[0] for word in words)
+        return sentence
